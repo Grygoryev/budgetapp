79827fe885d63ccc4976e7291c03da33117a1237
N.Sushko <n.sushko@ourgapps.com>
2019-05-15 14:27:18 +0300

    initial

---
+++ build/js/index.js
@@ -0,0 +1,13 @@
+(function enterWordController() {
+  let wordForm = document.querySelector('.enter-word'),
+    original = wordForm.wordOriginal,
+    article = wordForm.wordArticle,
+    addBtn = document.querySelector('js-add-word-btn')
+
+  let newWord = document.createElement('div'),
+      newWord.className = 'word'
+
+  addBtn.addEventListener('click', function() {
+
+  })
+})();
